73dcde9e7b848ffb12ed02c4032ceb06f95cdb68
Sahil-Kumar7253 <darkslayer7253@gmail.com>
2025-07-19 18:05:23 +0530

    Add Express Js

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 const http = require("http")
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req ,res) => {
+    return res.end("Home Page")
+})
+
+app.get("/about", (req,res)=>{
+    return res.end("My Name")
+})
+
+app.listen(8000, () => {
+    console.log("Server Started");
+})
+
+
+// Manually Handling Each Request
 
 //create an http server
-const myServer = http.createServer((req,res) => {
-    if(req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()} : ${req.url} New Req Recieved\n`;
-    const myUrl = url.parse(req.url,true);
-    console.log(myUrl);
-    fs.appendFile("log.txt", log,(err ,data) => {
-        switch(myUrl.pathname){
-            case "/":
-                res.end("Hello From Server");
-                break;
-            case "/about":
-                res.end("I Am Sahil Kumar");
-                break;
-            default:
-                res.end("404 Not Found");
-        }
-    }); 
-});
+//const myServer = http.createServer(app);
 
 //req stand for request
 //res stand for response
 
-myServer.listen(8000, () => {
-    console.log("Server Started");
-});
+// myServer.listen(8000, () => {
+//     console.log("Server Started");
+// });
 
+//const fs = require("fs");
+//const url = require("url");
+
+// function myHandler(req,res){
+//     if(req.url === "/favicon.ico") return res.end();
+//     const log = `${Date.now()} : ${req.url} New Req Recieved\n`;
+//     const myUrl = url.parse(req.url,true);
+//     fs.appendFile("log.txt", log,(err ,data) => {
+//         switch(myUrl.pathname){
+//             case "/":
+//                 res.end("Hello From Server");
+//                 break;
+//             case "/about":
+//                 res.end("I Am Sahil Kumar");
+//                 break;
+//             default:
+//                 res.end("404 Not Found");
+//         }
+//     });
+// }
  
